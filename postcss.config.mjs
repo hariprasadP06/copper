0e302f922dc2b4a14b423881a17cb86044780ddb
Hari prasad P <hariprasad.1si22cs404@gmail.com>
2025-04-16 14:53:30 +0530

    Add DaisyUI plugin and update configuration files

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+// export default config;
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
-
 export default config;
